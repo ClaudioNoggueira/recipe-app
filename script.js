f1ef2944cf14404497bca951ce0eff0eb728a3a6
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-01-10 15:41:06 -0300

    Getting random meal

---
+++ script.js
@@ -0,0 +1,37 @@
+const meals = document.getElementById("meals");
+
+getRandomMeal();
+
+async function getRandomMeal() {
+    const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+
+    const responseData = await response.json();
+    const randomMeal = responseData.meals[0];
+
+    addMeal(randomMeal, true);
+}
+
+async function getMealById(id) {
+    const meal = await fetch('www.themealdb.com/api/json/v1/1/lookup.php?i=' + id);
+}
+
+async function getMealsBySearch(term) {
+    const meals = await fetch('www.themealdb.com/api/json/v1/1/search.php?s=' + term);
+}
+
+function addMeal(mealData, random = false) {
+    const meal = document.createElement('div');
+    meal.classList.add('meal');
+
+    meal.innerHTML =
+        `<div class="meal-header">
+           ${random ? `<span class="random">Veja essa nova receita</span>` : ''}
+            <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
+        </div>
+        <div class="meal-body">
+            <h4>${mealData.strMeal}</h4>
+            <button class="favorite-btn"><i class="fas fa-heart"></i></button>
+        </div>`;
+
+    meals.appendChild(meal);
+}
